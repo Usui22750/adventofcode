58d3c9e742516d3801f606ebfb13745ebd4620be
Loic Rowe <loic.rowe@paylead.fr>
2022-12-06 11:11:18 +0100

    add solution j06

---
+++ adventofcode2022/j06/test.py
@@ -0,0 +1,28 @@
+import pytest
+from .day import find_marker, find_big_marker
+
+@pytest.mark.parametrize(
+    "line,expected",
+    [
+        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
+        ("nppdvjthqldpwncqszvftbrmjlhg", 6),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+    ]
+)
+def test_find_marker_4(line, expected):
+    assert find_marker(line, 4) == expected
+
+@pytest.mark.parametrize(
+    "line,expected",
+    [
+        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+        ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
+    ]
+)
+def test_find_marker_14(line, expected):
+    assert find_big_marker(line, 14) == expected
